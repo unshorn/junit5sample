cb89f9810730b0548671aaa37d5aa500c0bfb5a7
Shawn <unshorn@gmail.com>
2021-08-09 16:03:31 +1200

    Added files

---
+++ src/test/java/foo/AppTest.java
@@ -0,0 +1,25 @@
+package foo;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    @Test
+    public void appTest()
+    {
+        assertEquals(2, 2);
+    }
+
+
+}
